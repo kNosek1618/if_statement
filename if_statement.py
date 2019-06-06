3b2aabf72f7ade9360af29b99f984949d2af8f4e
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-06 16:49:47 +0200

    correct word

--- if_statement.py
+++ if_statement.py
@@ -28,6 +28,7 @@ if is_male and is_tall:
 elif is_male and not(is_tall):
     print("You are male and not tall")
 elif not(is_male) and is_tall:
-    print("You are not male and you are tall")
+    print("You are not male but you are tall")
 else:
     print("You are not a male and not tall")
+
