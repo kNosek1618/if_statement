55c7c438284d4bce299ec23844ed7762ffe0a359
kNosek1618 <k.nosek@ievent-management.pl>
2019-06-06 16:45:00 +0200

    elif

--- if_statement.py
+++ if_statement.py
@@ -1,4 +1,4 @@
-
+print("1.")
 #using if function
 is_male = True
 
@@ -6,3 +6,28 @@ if is_male:
     print("You are a male")
 else:
     print("You are not a male")
+
+
+print("2.")
+#if with two boolean
+is_male = True
+is_tall = False
+
+if is_male and is_tall:
+    print("You are a male and tall")
+else:
+    print("You are not a male or tall or both")
+
+print("3.")
+#if function with elif (else + if)
+is_male = True
+is_tall = True
+
+if is_male and is_tall:
+    print("You are a male and tall")
+elif is_male and not(is_tall):
+    print("You are male and not tall")
+elif not(is_male) and is_tall:
+    print("You are not male and you are tall")
+else:
+    print("You are not a male and not tall")
